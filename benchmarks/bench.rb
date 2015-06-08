62099bcb85dc103b8325cd53e66950334a1b373d
timcolonel <timothee.guerin@outlook.com>
2015-06-08 17:25:24 +0100

    Benchmarks

---
+++ benchmarks/bench.rb
@@ -0,0 +1,22 @@
+require 'benchmark/ips'
+$LOAD_PATH.push File.expand_path('../..', __FILE__)
+$LOAD_PATH.push File.expand_path('../../lib', __FILE__)
+require 'clin'
+
+
+Benchmark.ips do |x|
+  x.report('simple') do
+    require 'examples/simple'
+    SimpleCommand.parse('display Some -e "Even More"')
+  end
+
+  x.report('auto_option') do
+    require 'examples/auto_option'
+    AutoOptionCommand.parse('--eko="Lorem ipsum"')
+  end
+
+  x.report('nested_dispatcher') do
+    require 'examples/nested_dispatcher'
+    DispatchCommand.parse('you display Some --verbose -e More --times 3')
+  end
+end
--- examples/test.rb
+++ examples/test.rb
@@ -1,9 +1,19 @@
 $LOAD_PATH.push File.expand_path('../../lib', __FILE__)
 require 'clin'
+require 'json'
 
+def decode(str)
+  str.gsub(/([^\\](?:\\\\)*\\u[\da-f]{4})/i) do |m|
+    m[0...-6] + [m[-4..-1].to_i(16)].pack('U')
+  end
+end
 
-shell = Clin::Shell.new
-
-choice = shell.choose('What?', %w(france usa italy germany))
-
-puts 'YOu ' + choice
+a = []
+a << decode('1 This string does not have a unicode \\u escape.')
+a << decode('2 This string does not have a unicode \u005Cu escape.')
+a << decode('3 This string does not have a unicode \\\\u0075 escape.')
+a << decode('4 This string does not have a unicode \\\\\\u0075 escape.')
+a << JSON.parse('{"name": "This string does not have a unicode \\\\u0075 escape."}')
+a << JSON.parse('{"name": "This string does not have a unicode \\\\\\u0075 escape."}')
+a << JSON.parse(%({"value": #{'"some val \\a"'}}))['value']
+puts a
