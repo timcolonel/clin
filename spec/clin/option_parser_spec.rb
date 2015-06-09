630ad48d6d48b6d2b8dda200dc0555d8bc0e7352
timcolonel <timothee.guerin@outlook.com>
2015-06-09 17:24:53 +0100

    Spec

--- spec/clin/option_parser_spec.rb
+++ spec/clin/option_parser_spec.rb
@@ -1,7 +1,7 @@
 require 'clin'
 
 RSpec.describe Clin::OptionParser do
-  describe 'handle_unknown_option' do
+  describe '#handle_unknown_option' do
     let(:name) { :verbose }
     let(:value) { Faker::Lorem.word }
 
@@ -41,6 +41,62 @@ RSpec.describe Clin::OptionParser do
 
       it { expect(subject.skipped_options.size).to eq 2 }
       it { expect(subject.skipped_options).to eq [name, value] }
+    end
+  end
+
+  describe '#complete' do
+    let(:command) { double(:command) }
+    let(:value) { Faker::Lorem.word }
+    it 'keep the value when the value exists' do
+      parser = Clin::OptionParser.new(command, ['some'])
+      expect(parser.complete(value)).to eq(value)
+      expect(parser.instance_variable_get(:@argv)).to eq(['some'])
+    end
+
+    it 'get next argument value when the value is nil' do
+      parser = Clin::OptionParser.new(command, [value])
+      expect(parser.complete(nil)).to eq(value)
+      expect(parser.instance_variable_get(:@argv)).to eq([])
+    end
+
+    it 'return nil when value is nil and next argument is an option' do
+      parser = Clin::OptionParser.new(command, ['-o'])
+      expect(parser.complete(nil)).to be nil
+      expect(parser.instance_variable_get(:@argv)).to eq(['-o'])
+    end
+  end
+
+  describe '#parse_next' do
+    let(:command) { double(:command) }
+    context 'when option is a long option' do
+      subject { Clin::OptionParser.new(command, ['--opt=val']) }
+      before do
+        allow(subject).to receive(:parse_long)
+        subject.parse_next
+      end
+      it { expect(subject).to have_received(:parse_long).with('--opt', 'val') }
+      it { expect(subject.instance_variable_get(:@argv)).to be_empty }
+      it { expect(subject.instance_variable_get(:@arguments)).to be_empty }
+    end
+
+    context 'when option is a short option' do
+      subject { Clin::OptionParser.new(command, ['-oval']) }
+      before do
+        allow(subject).to receive(:parse_short)
+        subject.parse_next
+      end
+      it { expect(subject).to have_received(:parse_short).with('-o', 'val') }
+      it { expect(subject.instance_variable_get(:@argv)).to be_empty }
+      it { expect(subject.instance_variable_get(:@arguments)).to be_empty }
+    end
+
+    context 'when option is not an option' do
+      subject { Clin::OptionParser.new(command, ['not opt']) }
+      before do
+        subject.parse_next
+      end
+      it { expect(subject.instance_variable_get(:@argv)).to be_empty }
+      it { expect(subject.instance_variable_get(:@arguments)).to eq(['not opt']) }
     end
   end
 end
